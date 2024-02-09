df1fa7222901de1def12a8f3ecaa4b3724f1c83d
adedolapo-prog <shayne.doly94@gmail.com>
2024-02-09 07:29:54 -0800

    #error handling

--- CSharpCrash/Program.cs
+++ CSharpCrash/Program.cs
@@ -5,9 +5,22 @@ class Program
     public static void Main()
     {
         int age = 29;
+        double interest = 5.53;
+        char val = 'A';
+        string name = "Johnny";
+        int value1 = 5;
+        int value2 = 0;
 
-        PrintToConsole("Hello, Dolapo");
-        VariablesAndDataTypes(age);
+        PrintToConsole($"Hello, {name}");
+        VariablesAndDataTypes(age, interest);
+
+        try
+        {
+            DivisionFunction(value1, value2);
+        } catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message + " Error dividing by zero");
+        }
     }
 
     static void PrintToConsole(string parameter)
@@ -15,8 +28,14 @@ class Program
         Console.WriteLine($"{parameter}");
     }
 
-    static void VariablesAndDataTypes(int age)
+    static void VariablesAndDataTypes(int age, double interestRate)
     {
-        Console.WriteLine($"{age}");
+        Console.WriteLine($"How old are you? {age}?");
+        Console.WriteLine($"Today's interest rate is {interestRate}.");
+    }
+
+    static void DivisionFunction(int num1, int num2)
+    {
+        Console.WriteLine(num1 / num2);
     }
 }
