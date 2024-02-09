f2d3612677335d6e49ade07c1d3a828509ef14ec
USER <USER@VASUDHA>
2024-02-09 06:52:39 -0800

    Add project files.

---
+++ CSharpCrash/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int age = 29;
+
+        PrintToConsole("Hello, Dolapo");
+        VariablesAndDataTypes(age);
+    }
+
+    static void PrintToConsole(string parameter)
+    {
+        Console.WriteLine($"{parameter}");
+    }
+
+    static void VariablesAndDataTypes(int age)
+    {
+        Console.WriteLine($"{age}");
+    }
+}
